749bb21cf524c21a2d16d50c40904c0273c6636b
Nesterov Alexey <vertigra.developer@gmail.com>
2017-08-14 18:00:39 +1000

    Test commit

--- NetTelebot.Commands.TestApplication/frmMain.cs
+++ NetTelebot.Commands.TestApplication/frmMain.cs
@@ -34,11 +34,13 @@ namespace NetTelebot.Commands.TestApplication
         private void btnStart_Click(object sender, EventArgs e)
         {
             mBot.Start();
+            txtLog.Text += $"Bot Start \r\n";
         }
 
         private void btnStop_Click(object sender, EventArgs e)
         {
             mBot.Stop();
+            txtLog.Text += $"Bot Stop \r\n";
         }
 
         public TelegramCredentials GetTelegramCredential(string botAlias)
